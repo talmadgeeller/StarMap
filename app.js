e044a2c38803b248f5f1efe7027e10e3c002c1e5
talmadgeeller <inventor98@gmail.com>
2022-03-07 22:05:54 -0500

    Added globe line color adjustment

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const customizations = document.querySelector('#customizations');
 document.querySelector("#globe").addEventListener('change', function () {
     globeLabel.textContent = this.value.toString();
     document.documentElement.style.setProperty('--globe-fill', this.value);
+    if (hexToHSL(globeLabel.textContent).l >= 0.6)
+        document.documentElement.style.setProperty('--graticule-fill', "#000000");
+    else
+        document.documentElement.style.setProperty('--graticule-fill', "#ffffff");
 });
 
 document.querySelector("#globeOutline").addEventListener('change', function () {
@@ -68,3 +72,30 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
     $("#copyTextButton").hide();
 })
+
+function hexToHSL(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    r = parseInt(result[1], 16);
+    g = parseInt(result[2], 16);
+    b = parseInt(result[3], 16);
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+    var HSL = new Object();
+    HSL['h'] = h;
+    HSL['s'] = s;
+    HSL['l'] = l;
+    return HSL;
+}
